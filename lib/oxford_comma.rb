588604f8fa9a26314f5cd54e20e76832e9a39e78
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-03-25 21:11:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -28,12 +28,3 @@ end
   
 #array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
 
-#def oxford_comma(array)
- # if array.length == 1
-  #  array.join
-  #elsif array.length == 2
-   # array.join(" and ")
-  #else
-   # array.join(", ") << ", and " << array[-1]
- # end
-#end
