e4ac17b302578a1d8e5b3da0d459de2d8d75ca85
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-03-25 21:09:43 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,8 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   else
-  x = array.pop
-  array.join(", ") << ", and " << x
+   x = array.pop
+   array.join(", ") << ", and " << x
   end
 end
 
